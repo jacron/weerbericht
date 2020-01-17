10407e680b94d8270f0672323b1e88f3b14dc4eb
jhsm croonen <info@jcroonen.nl>
2020-01-17 20:54:51 +0100

    less permissions, added new kaart

--- popup.js
+++ popup.js
@@ -1,17 +1,9 @@
-const REACT_APP_WEER_API = 'http://weerlive.nl/api/json-10min.php?locatie=zeldert';
-const REACT_APP_KNMI_WINDKRACHT_KAART = 'http://cdn.knmi.nl/knmi/map/page/weer/actueel-weer/windkracht.png';
-const REACT_APP_KNMI_TEMPERATUUR_KAART = 'http://cdn.knmi.nl/knmi/map/page/weer/actueel-weer/temperatuur.png';
-
-function shortLocaleTime() {
-    const days = [
-        'Zon', 'Ma', 'Di', 'Wo', 'Do', 'Vr', 'Za',
-    ];
-    const now = new Date();
-    const day = days[now.getDay()];
-    const time = now.toLocaleTimeString('nl-NL');
-    const nparts = time.split(':');
-    return day + ' ' + nparts[0] + ':' + nparts[1] + ' u.';
-}
+const WEER_API = 'http://weerlive.nl/api/json-10min.php?locatie=zeldert';
+const kaart = {
+    buien: 'https://cdn.knmi.nl/knmi/map/page/weer/actueel-weer/neerslagradar/WWWRADARBFT_loop.gif',
+    wind: 'http://cdn.knmi.nl/knmi/map/page/weer/actueel-weer/windkracht.png',
+    temperatuur: 'http://cdn.knmi.nl/knmi/map/page/weer/actueel-weer/temperatuur.png',
+};
 
 function getIcon(image) {
     return './icons/' + image + '.png';
@@ -28,26 +20,19 @@ function getAlarm(data) {
 }
 
 function getKaart(type) {
-    if (type === 'wind') return REACT_APP_KNMI_WINDKRACHT_KAART;
-    if (type === 'temperatuur') return REACT_APP_KNMI_TEMPERATUUR_KAART;
+    return kaart[type];
 }
 
 function fillForm(data) {
     const bindings = [
-        // ['plaats', 'plaats'],
         ['verw', 'verw'],
         ['temp', 'temp'],
-        // ['d0tmin', 'd0tmin'],
-        // ['d0tmax', 'd0tmax'],
-        // ['winds', 'winds'],
-        // ['windr', 'windr'],
         ['sup', 'sup'],
         ['sunder', 'sunder'],
     ];
     for (const [id, field] of bindings) {
         document.getElementById(id).innerText = data[field];
     }
-    // document.getElementById('alarmtxt').innerText
     const funBindings = [
         // ['created', shortLocaleTime, null],
         ['alarmtxt', getAlarm, data],
@@ -59,15 +44,15 @@ function fillForm(data) {
         ['icon', getIcon, data.image],
         ['windkracht-kaart', getKaart, 'wind'],
         ['temperatuur-kaart', getKaart, 'temperatuur'],
+        ['buien-kaart', getKaart, 'buien'],
     ];
     for (const [id, fun, arg] of imgBindings) {
         document.getElementById(id).src = fun(arg);
     }
-    // document.getElementById('icon').src = getIcon(data.image);
 }
 
 function initForm() {
-    fetch(REACT_APP_WEER_API)
+    fetch(WEER_API)
         .then(res => res.json())
         .then((result) => {
             fillForm(result.liveweer[0])
